b3670b8e4245b9c2a2a96fbf481ac1a2cbe0f2e4
DopeCodes254 <njengajames724@gmail.com>
2022-01-26 03:33:24 +0300

    added colorFlipper files

---
+++ colorFlipper/app.js
@@ -0,0 +1,13 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const colorName = document.querySelector(".color");
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
+
+btn.addEventListener('click', function(){
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    colorName.textContent = colors[randomNumber];
+});
---
+++ colorFlipper/hex.js
@@ -0,0 +1,18 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById('btn');
+const colorName = document.querySelector('.color');
+
+
+
+
+btn.addEventListener('click', function(){
+    let color = "#";
+    for(let i = 0; i < 6; i++){   
+        let randomNumber = Math.floor(Math.random()*hex.length);
+        color += hex[randomNumber];
+    }
+    document.body.style.backgroundColor = color;
+    colorName.textContent = color;
+});
+
+
